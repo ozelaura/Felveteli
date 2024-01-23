e1f90d5f0aaea9d517a1065c70634527e3406472
ozelaura <70641602+ozelaura@users.noreply.github.com>
2024-01-23 13:48:50 +0100

    Add files via upload

--- Felveteli/Felveteli/MainWindow.xaml.cs
+++ Felveteli/Felveteli/MainWindow.xaml.cs
@@ -20,9 +20,45 @@ namespace Felveteli
     /// </summary>
     public partial class MainWindow : Window
     {
+        Felvetelizo felvetelizoAdatai;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public MainWindow(Felvetelizo ujdiak) : this()
+        {
+            this.felvetelizoAdatai = ujdiak;
+
+            this.Title = $"{felvetelizoAdatai.Neve} adataink rögzítése";
+            txtNeve.Text = felvetelizoAdatai.Neve;
+        }
+
+        private void btnRogzit_Click(object sender, RoutedEventArgs e)
+        {
+            felvetelizoAdatai.OM_Azonosito = txtOMazonosito.Text;
+            felvetelizoAdatai.Neve = txtNeve.Text;
+            felvetelizoAdatai.EretesitesiCime = txtCim.Text;
+            felvetelizoAdatai.Email = txtEmail.Text;
+            felvetelizoAdatai.SzuletesiDatum = Convert.ToDateTime(txtSzuletesiDatum.Text);
+
+            try
+            {
+                this.felvetelizoAdatai.Matematika = int.Parse(txtMatematika.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Nem számformátum!");
+                return;
+            }
+            if (felvetelizoAdatai.Matematika < 0 || felvetelizoAdatai > 50)
+            {
+                MessageBox.Show("Nem lehet ennyi pontja!");
+                return;
+            }
+
+            this.felvetelizoAdatai.Magyar = int.Parse(txtMagyar.Text);
+            Close();
         }
     }
 }
